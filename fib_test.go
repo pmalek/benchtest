51b7ef8cf4a30beee3710f76586edd3ac2e1075f
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2023-10-19 15:23:31 -0700

    make it slow

--- fib_test.go
+++ fib_test.go
@@ -6,18 +6,18 @@ import (
 
 func BenchmarkFib10(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = Fib(13)
+		_ = Fib(15)
 	}
 }
 
 func BenchmarkFib20(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = Fib(23)
+		_ = Fib(26)
 	}
 }
 
 func BenchmarkFib25(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = Fib(30)
+		_ = Fib(35)
 	}
 }
