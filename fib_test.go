a8e90072b8f7dfd8592c4d065b089cc2b18ed278
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2023-10-19 18:47:46 -0700

    more permissions

--- fib_test.go
+++ fib_test.go
@@ -6,18 +6,18 @@ import (
 
 func BenchmarkFib10(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = Fib(100)
+		_ = Fib(1)
 	}
 }
 
 func BenchmarkFib20(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = Fib(100)
+		_ = Fib(1)
 	}
 }
 
 func BenchmarkFib25(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = Fib(100)
+		_ = Fib(1)
 	}
 }
